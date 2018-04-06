7a10be897b7b152ce2c6d5ec4990cddfa7f10d47
Matej <zlekrat@gmail.com>
2018-04-06 11:32:28 +0200

    More improvements to argparser

--- src/core/args.py
+++ src/core/args.py
@@ -7,11 +7,13 @@ About: Argument parser
 
 import sys, os
 
+# Checks number of args
 def check_args(argv):
     if (len(argv) != 3):
         print("Usage: python3 Path_to_TIFF Path_to_CSV\n")
         exit()
 
+# Gets TIFF file from args
 def get_path_img(argv):
     check_args(argv)
     if (os.path.exists(argv[1])):
@@ -20,13 +22,11 @@ def get_path_img(argv):
         print("Usage: python3 Path_to_TIFF Path_to_CSV\n")
         exit()
 
+# Gets CSV from args
 def get_path_csv(argv):
     open(argv[2], "w+")
     return argv[2]
 
-print(sys.argv)
-print(get_path_img(sys.argv))
-print(get_path_csv(sys.argv))
 
 
 
