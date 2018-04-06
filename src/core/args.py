c3994bb008132b37205f017d180da8a4c80be05b
Matej <zlekrat@gmail.com>
2018-04-06 11:30:28 +0200

    Improved argparser

--- src/core/args.py
+++ src/core/args.py
@@ -16,10 +16,17 @@ def get_path_img(argv):
     check_args(argv)
     if (os.path.exists(argv[1])):
         return argv[1]
+    else:
+        print("Usage: python3 Path_to_TIFF Path_to_CSV\n")
+        exit()
 
 def get_path_csv(argv):
-    check_args(argv)
+    open(argv[2], "w+")
     return argv[2]
+
+print(sys.argv)
+print(get_path_img(sys.argv))
+print(get_path_csv(sys.argv))
 
 
 
